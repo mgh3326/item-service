68f57d78c19f959a6b1a2d8eb6b2c32dd58c508e
robin <robin@watcha.com>
2022-08-14 16:57:43 +0900

    상품 상세

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -26,7 +26,13 @@ public class BasicItemController {
     return "basic/items";
 
   }
+  @GetMapping("/{itemId}") public String item(@PathVariable Long itemId, Model model) {
 
+    Item item = itemRepository.findById(itemId);
+
+    model.addAttribute("item", item);
+
+    return "basic/item"; }
   /**
    * 테스트용 데이터 추가
    */
